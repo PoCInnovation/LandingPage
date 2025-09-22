90f7c6d7c6546ecbb1f682a21ca647f50b843948
Lucas Leclerc <lucasleclerc74@gmail.com>
2025-09-22 23:31:05 +0200

    add: link to home page on navbar logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   metadataBase: new URL('https://www.poc-innovation.fr'),
   icons: {
-    icon: '/logo_black.svg',
+    icon: '/logo_poc.png',
   },
   title: 'PoC - Centre d\'innovation étudiant',
   description:
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,14 +34,16 @@ function GlassyNavbar() {
       <div
         className={`fixed top-4 left-1/2 transform -translate-x-1/2 z-50 flex bg-[#E2E2E21A]/80 border-foreground/10 rounded-4xl backdrop-blur-3xl shadow-lg shadow-white/10 px-2 sm:pl-4 sm:pr-2 md:pr-2 items-center gap-1 sm:gap-3 md:gap-6 transition-transform duration-300 h-14 sm:h-16 w-fit min-w-[280px] sm:min-w-[355px] md:min-w-[460px] ${isVisible ? 'translate-y-0' : '-translate-y-[calc(100%+2rem)]'}`}
       >
-        <Image
-          src='/logo_poc.png'
-          alt='Logo PoC'
-          width={80}
-          height={80}
-          className='object-contain w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 opacity-0 animate-[fadeIn_0.3s_ease-in-out_forwards] ml-1 sm:ml-2 md:ml-3 mr-2 sm:mr-3 md:mr-4'
-          priority
-        />
+        <Link href='/' className='cursor-pointer group'>
+          <Image
+            src='/logo_poc.png'
+            alt='Logo PoC'
+            width={80}
+            height={80}
+            className='object-contain w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 opacity-0 animate-[fadeIn_0.3s_ease-in-out_forwards] ml-1 sm:ml-2 md:ml-3 mr-2 sm:mr-3 md:mr-4'
+            priority
+          />
+        </Link>
         <div className='flex items-center gap-3 sm:gap-4 md:gap-6 lg:gap-8'>
           <Link href='/' className='cursor-pointer group'>
             <button className='cursor-pointer' type='button'>
